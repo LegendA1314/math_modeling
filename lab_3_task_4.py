cd9c22a1bb67020c1022f304d9392e06f5c24c15
Ильяс <92121664+LegendA1314@users.noreply.github.com>
2021-11-25 21:12:17 +0000

    45

--- lab_3_task_4.py
+++ lab_3_task_4.py
@@ -7,12 +7,12 @@ st = 0
 A = np.zeros((N, M))
 
 if st == 0:
-  for i in st,N
-  for j in st,M
+  for i in 0,N
+  for j in 0,M
   print(math.sin(N · (i+1) + M · (j + 1)))
 else:
-  for i in st,N
-  for j in st,M
+  for i in 1,N
+  for j in 1,M
   print(math.sin(N · i + M · j))
 
 
