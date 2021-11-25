66429502d562368aff8b51edb6591649ab92397f
Ильяс <92121664+LegendA1314@users.noreply.github.com>
2021-11-25 21:11:53 +0000

    45

--- lab_3_task_4.py
+++ lab_3_task_4.py
@@ -1,10 +1,18 @@
 import numpy as np 
+import math
+
 N = 5
 M = 12
 st = 0
 A = np.zeros((N, M))
 
 if st == 0:
-  for i=st
-  print
+  for i in st,N
+  for j in st,M
+  print(math.sin(N · (i+1) + M · (j + 1)))
+else:
+  for i in st,N
+  for j in st,M
+  print(math.sin(N · i + M · j))
+
 
