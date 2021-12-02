97da4233a521672b43f11716d1ba0c7e73ab64c8
Ильяс <92121664+LegendA1314@users.noreply.github.com>
2021-12-02 21:49:06 +0000

    n

---
+++ lec_3_dop_3.py
@@ -0,0 +1,14 @@
+a = np.zeros((2, 2))
+b = np.zeros((2, 2))
+c = np.zeros((2, 2))
+
+print ("введите элементы массива 1")
+
+for i in range(1)
+ for j in range(1)
+  print("Элемент i, j:",i,j)
+  a[i,j] = input()
+
+print(a)
+
+
