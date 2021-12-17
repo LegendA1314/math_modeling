529d12560a375e4c0a3649db439bf9f99b3b9781
Ильяс <92121664+LegendA1314@users.noreply.github.com>
2021-12-17 14:41:58 +0000

    h

--- lab_4_task_5.py
+++ lab_4_task_5.py
@@ -1,13 +1,29 @@
+print('Введите число, которому соответствует фигура 1 - круг, 2 - треугольник, 3  - прямоугольник ')
 p = int(input())
 
 if p == 1:
   a = int(input())
   def krug (a):
     v = a**2 *3.14
-    print (v)
+    return a
+  print(krug(a))
 elif p == 2:
   a = int(input())
   b = int(input())
-  c = int(input())
-  
-else p == 3
+  def trey (a, b):
+    m = a * b * 0.5
+    return m
+  print(trey(a, b))
+elif p == 3:
+  a = int(input())
+  b = int(input())
+  def pryam (a, b):
+    l = a * b
+    return l
+  print(pryam(a,b))
+else:
+  a = int(input())
+  def af (a):
+    a = "НЕТ "
+    return a
+  print (af(a))
