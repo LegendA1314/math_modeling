5cb7c8233ca976bddb3ca03c3a22994464867643
Ильяс <92121664+LegendA1314@users.noreply.github.com>
2022-02-11 13:58:37 +0000

    g

--- lec_7_task_2.py
+++ lec_7_task_2.py
@@ -1,26 +1,26 @@
-import numpy as np
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import numpy as np
 
-	
 fig, ax = plt.subplots()
-	
-anim_object, = plt.plot([], [], '-', lw=2)
+krug, = plt.plot([], [], '-', color='g',)
 
-xdata, ydata = [], []
+e = 20
+plt.axis('equal')
+ax.set_xlim(-e, e)
+ax.set_ylim(-e, e) 
 
-ax.set_xlim(0, 2*np.pi)
-ax.set_ylim(-1, 1)
+def circle_big (b, f, t):
+    l = f * t * (np.pi / 360) 
+    R = l * t
+    x = R * np.cos(b)
+    y = R * np.sin(b)
+    return x, y
 
-def update(frame): 
-	
-    xdata.append(R * np.cos(frame)) 
-	
-    ydata.append(R * np.sin(frame)) 
-	
-    anim_object.set_data(xdata, ydata) 
-	
-    return anim_object,
+def a(i):
+    krug.set_data(circle_big(b=np.linspace(0, 2*np.pi, 300), f=1, t=i))
 
-ani = FuncAnimation(fig, update,	frames=np.arange(0, 2*np.pi, 0.1), interval=100)         
-ani.save('lec_7_task_2.py')
+ani = animation.FuncAnimation(fig, a, frames=200, interval=3 )
+
+plt.show()
+
