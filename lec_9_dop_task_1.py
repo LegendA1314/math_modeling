6fa02502aa457dce0cdcc26ac970258851b4c311
Ильяс <92121664+LegendA1314@users.noreply.github.com>
2022-02-25 14:52:49 +0000

    A

--- lec_9_dop_task_1.py
+++ lec_9_dop_task_1.py
@@ -6,11 +6,12 @@ t = np.arange(1, 25, 0.1)
 
 
 def razmer(S, t):
-  dsdt = 2 * r *(np.pi * S * r * S * np.cos (np.pi - (t - 12) / t))**2
-  return dsdt
+  o = k * (s/np.pi)**0.5 * r *s * np.cos((np.pi/12)*(t-12))
+  return o
   
 S = 1600
 r = 1360
+k = 2
 
 solve= odeint(razmer, S, t)
 
