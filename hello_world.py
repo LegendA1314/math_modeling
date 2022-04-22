3bfccf2c74708a7eabc87d16cb39edbbbfd818d6
Ильяс <92121664+LegendA1314@users.noreply.github.com>
2022-04-22 15:06:50 +0000

    1

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,31 @@
-print('aga')
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as plt3d
 
+# Создание пространства для анимации
+fig = plt.figure()
+ax = plt3d.Axes3D(fig)
+
+# Определение параметров кривой
+t = np.arange(0.01, 4*np.pi, 0.01)
+R = 1
+
+# Параметрическое задание пространственной кривой
+x = R * np.cos(t)
+y = R * t**0.5
+z = R * np.log10(t)
+
+# Построение пространственной кривой
+ax.plot(x, y, z, label='Dich')
+
+ax.legend()
+
+# Подписи осей
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Подпись графика
+ax.set_title('3D Test')
+
+plt.show()
